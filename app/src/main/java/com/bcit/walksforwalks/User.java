fccc4237906de9a9c807fc53db1d48f02ec09cc1
Victor Cho <victorcho@outlook.com>
2021-03-21 18:09:10 -0700

    User Login and Authentication Version 1.

---
+++ app/src/main/java/com/bcit/walksforwalks/User.java
@@ -0,0 +1,15 @@
+package com.bcit.walksforwalks;
+
+public class User {
+    public String fullName;
+    public String email;
+
+    public User() {
+
+    }
+
+    public User(String fullName, String email) {
+        this.fullName = fullName;
+        this.email = email;
+    }
+}
