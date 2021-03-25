a7681780cb38adf2ea167e228fdb5e44cc05654a
Ralph Kilian <rkilian1@my.bcit.ca>
2021-03-24 17:12:30 -0700

    profile fragment skeleton

---
+++ app/src/main/java/com/bcit/walksforwalks/ProfileFragment.java
@@ -0,0 +1,28 @@
+package com.bcit.walksforwalks;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+    }
+
+    public ProfileFragment(int contentLayoutId) {
+        super(contentLayoutId);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
